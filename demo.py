ecfee9574f68c08e323aefa88eea69230353ae1d
Stav Sapir <stavsap@gmail.com>
2024-10-26 20:04:51 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 import whisper
 
 # whisper has multiple models that you can load as per size and requirements
-model = whisper.load_model("small.en")
+model = whisper.load_model("large-v3", device="cuda", in_memory=True, download_root="./models")
 
 # path to the audio file you want to transcribe
 PATH = "audio.mp3"
 
 result = model.transcribe(PATH)
+print("----------------------------------")
 print(result["text"])
